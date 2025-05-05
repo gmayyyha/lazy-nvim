377857924b87b60ff4329522dd7f096a3c24adb8
Yanhu Cao <gmayyyha@gmail.com>
2025-05-05 19:45:12 +0800

    codecompanion: add gemini support
    Signed-off-by: Yanhu Cao <gmayyyha@gmail.com>

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -11,6 +11,27 @@ return {
         },
     },
     config = function()
+        local default_model = "gemini-2.5-flash-preview-04-17"
+        local available_models = {
+            "gemini-2.5-flash-preview-04-17",
+            "google/gemini-2.0-flash-001",
+            "google/gemini-2.5-pro-preview-03-25",
+            "anthropic/claude-3.7-sonnet",
+            "anthropic/claude-3.5-sonnet",
+            "openai/gpt-4o-mini",
+        }
+        local current_model = default_model
+
+        local function select_model()
+            vim.ui.select(available_models, {
+                prompt = "Select  Model:",
+            }, function(choice)
+                if choice then
+                    current_model = choice
+                    vim.notify("Selected model: " .. current_model)
+                end
+            end)
+        end
         require("codecompanion").setup({
             opts = {
                 -- Set debug logging
@@ -29,21 +50,21 @@ return {
                         },
                     })
                 end,
-                siliconflow = function()
-                    return require("codecompanion.adapters").extend("deepseek", {
-                        name = "siliconflow",
-                        url = "https://api.siliconflow.com/v1/chat/completions",
+                google = function()
+                    return require("codecompanion.adapters").extend("openai_compatible", {
+                        name = "google",
+                        url = "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
                         env = {
                             api_key = function()
-                                return os.getenv("SILICONFLOW_KEY")
+                                return os.getenv("GEMINI_KEY")
                             end,
                         },
                         schema = {
                             model = {
-                                default = "deepseek-ai/DeepSeek-R1",
+                                default = current_model,
                                 choices = {
-                                    ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
-                                    "deepseek-ai/DeepSeek-V3",
+                                    ["gemini-2.5-flash-preview-04-17"] = { opts = { can_reason = true } },
+                                    "gemini-2.5-pro-preview-03-25",
                                 },
                             },
                         },
@@ -67,19 +88,23 @@ return {
                 end,
             },
             strategies = {
-                chat = { adapter = "siliconflow" },
-                inline = { adapter = "siliconflow" },
+                chat = { adapter = "google" },
+                inline = { adapter = "google" },
                 agent = { adapter = "siliconflow" },
             },
         })
 
         local keymap = vim.keymap.set
 
-        keymap({ "n", "v", "x" }, "<leader>ai", function()
-            require("codecompanion").toggle()
-        end)
-
         keymap({ "n", "v", "x" }, "<leader>cp", ":CodeCompanionActions<CR>")
+
+        keymap({ "n", "v" }, "<leader>ck", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+        keymap({ "n", "v" }, "<leader>ai", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+        keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+        keymap("n", "<leader>cm", select_model, { desc = "Select Gemini Model" })
+        -- Expand 'cc' into 'CodeCompanion' in the command line
+        vim.cmd([[cab cc CodeCompanion]])
 
         local fidget = require("fidget")
         local handler
