020035bfe8efa7d8fe7be6c6e7763c78c01720a5
Yanhu Cao <gmayyyha@gmail.com>
2025-05-07 07:58:53 +0800

    feat(codecompanion): Implement chat save/load and refactor setup
    Implement `CodeCompanionSave` and `CodeCompanionLoad` user commands for persisting chat buffers using fzf-lua for selection.
    Add explicit adapter configurations for Anthropic, OpenAI, Gemini, Deepseek, and Ollama.
    Refactor the `codecompanion.setup` configuration to utilize the new adapter structure and set default strategies and display options.
    Introduce a `prompt_library` with default prompts for buffer editing and code review.
    Update keymaps to a new `<leader>a*` scheme.
    
    Signed-off-by: Yanhu Cao <gmayyyha@gmail.com>

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,3 +1,170 @@
+--- Anthropic config for CodeCompanion.
+local anthropic_fn = function()
+    local anthropic_config = {
+        env = {
+            api_key = function()
+                return os.getenv("ANTHROPIC_KEY")
+            end,
+        },
+    }
+    return require("codecompanion.adapters").extend("anthropic", anthropic_config)
+end
+
+--- OpenAI config for CodeCompanion.
+local openai_fn = function()
+    local openai_config = {
+        env = { api_key = "cmd:op read op://Personal/OpenAI/tokens/neovim --no-newline" },
+    }
+    return require("codecompanion.adapters").extend("openai", openai_config)
+end
+
+--- Gemini config for CodeCompanion.
+local gemini_fn = function()
+    local gemini_config = {
+        env = {
+            api_key = function()
+                return os.getenv("GEMINI_KEY")
+            end,
+        },
+        schema = {
+            model = {
+                default = "gemini-2.5-flash-preview-04-17",
+            },
+            num_ctx = {
+                default = 16386,
+            },
+            num_predict = {
+                default = -1,
+            },
+        },
+    }
+    return require("codecompanion.adapters").extend("gemini", gemini_config)
+end
+
+--- Deepseek config for CodeCompanion
+local deepseek_fn = function()
+    return require("codecompanion.adapters").extend("openai_compatible", {
+        name = "deepseek",
+        env = {
+            url = "https://api.deepseek.com",
+            api_key = function()
+                return os.getenv("DEEPSEEK_KEY")
+            end,
+        },
+    })
+end
+
+--- Ollama config for CodeCompanion.
+local ollama_fn = function()
+    return require("codecompanion.adapters").extend("ollama", {
+        schema = {
+            model = {
+                default = "llama3.1:8b",
+                -- default = "codellama:7b",
+            },
+            num_ctx = {
+                default = 16384,
+            },
+            num_predict = {
+                default = -1,
+            },
+        },
+    })
+end
+
+local supported_adapters = {
+    anthropic = anthropic_fn,
+    openai = openai_fn,
+    gemini = gemini_fn,
+    deepseek = deepseek_fn,
+    ollama = ollama_fn,
+}
+
+local function save_path()
+    local Path = require("plenary.path")
+    local p = Path:new(vim.fn.stdpath("data") .. "/codecompanion_chats")
+    p:mkdir({ parents = true })
+    return p
+end
+
+--- Load a saved codecompanion.nvim chat file into a new CodeCompanion chat buffer.
+--- Usage: CodeCompanionLoad
+vim.api.nvim_create_user_command("CodeCompanionLoad", function()
+    local fzf = require("fzf-lua")
+
+    local function select_adapter(filepath)
+        local adapters = vim.tbl_keys(supported_adapters)
+
+        fzf.fzf_exec(adapters, {
+            prompt = "Select CodeCompanion Adapter> ",
+            actions = {
+                ["default"] = function(selected)
+                    local adapter = selected[1]
+                    -- Open new CodeCompanion chat with selected adapter
+                    vim.cmd("CodeCompanionChat " .. adapter)
+
+                    -- Read contents of saved chat file
+                    local lines = vim.fn.readfile(filepath)
+
+                    -- Get the current buffer (which should be the new CodeCompanion chat)
+                    local current_buf = vim.api.nvim_get_current_buf()
+
+                    -- Paste contents into the new chat buffer
+                    vim.api.nvim_buf_set_lines(current_buf, 0, -1, false, lines)
+                end,
+            },
+        })
+    end
+
+    local function start_picker()
+        local files = vim.fn.glob(save_path() .. "/*", false, true)
+
+        fzf.fzf_exec(files, {
+            prompt = "Saved CodeCompanion Chats | <c-r>: remove >",
+            previewer = "builtin",
+            actions = {
+                ["default"] = function(selected)
+                    if #selected > 0 then
+                        local filepath = selected[1]
+                        select_adapter(filepath)
+                    end
+                end,
+                ["ctrl-r"] = function(selected)
+                    if #selected > 0 then
+                        local filepath = selected[1]
+                        os.remove(filepath)
+                        -- Refresh the picker
+                        start_picker()
+                    end
+                end,
+            },
+        })
+    end
+
+    start_picker()
+end, {})
+
+--- Save the current codecompanion.nvim chat buffer to a file in the save_folder.
+--- Usage: CodeCompanionSave <filename>.md
+---@param opts table
+vim.api.nvim_create_user_command("CodeCompanionSave", function(opts)
+    local codecompanion = require("codecompanion")
+    local success, chat = pcall(function()
+        return codecompanion.buf_get_chat(0)
+    end)
+    if not success or chat == nil then
+        vim.notify("CodeCompanionSave should only be called from CodeCompanion chat buffers", vim.log.levels.ERROR)
+        return
+    end
+    if #opts.fargs == 0 then
+        vim.notify("CodeCompanionSave requires at least 1 arg to make a file name", vim.log.levels.ERROR)
+    end
+    local save_name = table.concat(opts.fargs, "-") .. ".md"
+    local save_file = save_path():joinpath(save_name)
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    save_file:write(table.concat(lines, "\n"), "w")
+end, { nargs = "*" })
+
 return {
     "olimorris/codecompanion.nvim",
     opts = {},
@@ -11,100 +178,138 @@ return {
         },
     },
     config = function()
-        local default_model = "gemini-2.5-flash-preview-04-17"
-        local available_models = {
-            "gemini-2.5-flash-preview-04-17",
-            "google/gemini-2.0-flash-001",
-            "google/gemini-2.5-pro-preview-03-25",
-            "anthropic/claude-3.7-sonnet",
-            "anthropic/claude-3.5-sonnet",
-            "openai/gpt-4o-mini",
-        }
-        local current_model = default_model
-
-        local function select_model()
-            vim.ui.select(available_models, {
-                prompt = "Select  Model:",
-            }, function(choice)
-                if choice then
-                    current_model = choice
-                    vim.notify("Selected model: " .. current_model)
-                end
-            end)
-        end
         require("codecompanion").setup({
             opts = {
                 -- Set debug logging
                 show_defaults = true,
-                log_level = "DEBUG",
+                --                log_level = "DEBUG",
             },
-            adapters = {
-                deepseek = function()
-                    return require("codecompanion.adapters").extend("openai_compatible", {
-                        name = "deepseek",
-                        env = {
-                            url = "https://api.deepseek.com",
-                            api_key = function()
-                                return os.getenv("DEEPSEEK_KEY")
-                            end,
-                        },
-                    })
-                end,
-                google = function()
-                    return require("codecompanion.adapters").extend("openai_compatible", {
-                        name = "google",
-                        url = "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
-                        env = {
-                            api_key = function()
-                                return os.getenv("GEMINI_KEY")
-                            end,
-                        },
-                        schema = {
-                            model = {
-                                default = current_model,
-                                choices = {
-                                    ["gemini-2.5-flash-preview-04-17"] = { opts = { can_reason = true } },
-                                    "gemini-2.5-pro-preview-03-25",
-                                },
-                            },
-                        },
-                    })
-                end,
-                deepseek_ollama = function()
-                    return require("codecompanion.adapters").extend("ollama", {
-                        name = "ollama-r1-14b",
-                        schema = {
-                            model = {
-                                default = "deepseek-r1:14b",
-                            },
-                            num_ctx = {
-                                default = 16384,
-                            },
-                            num_predict = {
-                                default = -1,
-                            },
-                        },
-                    })
-                end,
-            },
+            adapters = supported_adapters,
             strategies = {
-                chat = { adapter = "google" },
-                inline = { adapter = "google" },
-                agent = { adapter = "siliconflow" },
+                chat = {
+                    adapter = "gemini",
+
+                    slash_commands = {
+                        ["buffer"] = {
+                            opts = {
+                                provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+                            },
+                        },
+
+                        ["file"] = {
+                            opts = {
+                                provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+                            },
+                        },
+
+                        ["help"] = {
+                            opts = {
+                                provider = "fzf_lua", -- telescope|mini_pick|fzf_lua
+                            },
+                        },
+
+                        ["symbols"] = {
+                            opts = {
+                                provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+                            },
+                        },
+                    },
+                },
+                inline = { adapter = "gemini" },
+                agent = { adapter = "gemini" },
+                cmd = { adapter = "gemini" },
+            },
+            display = {
+                chat = {
+                    show_settings = true,
+                },
+                action_palette = {
+                    provider = "default", -- default|telescope|mini_pick
+                },
+                diff = {
+                    provider = "default", -- default|mini_diff
+                },
+            },
+            prompt_library = {
+                -- https://github.com/olimorris/codecompanion.nvim/blob/main/doc/RECIPES.md
+                ["Edit current buffer"] = {
+                    strategy = "chat",
+                    description = "Edit the current buffer",
+                    prompts = {
+                        {
+                            role = "system",
+                            content = "You are an AI assistant with access to the user's current code buffer",
+                        },
+                        {
+                            role = "user",
+                            content = "Please make the following changes using #buffer with @editor\n\n",
+                        },
+                    },
+                },
+                ["Code review"] = {
+                    strategy = "chat",
+                    description = "Code review",
+                    prompts = {
+                        {
+                            role = "system",
+                            content = [[Analyze the code for:
+
+### CODE QUALITY
+* Function and variable naming (clarity and consistency)
+* Code organization and structure
+* Documentation and comments
+* Consistent formatting and style
+
+### RELIABILITY
+* Error handling and edge cases
+* Resource management
+* Input validation
+
+### MAINTAINABILITY
+* Code duplication (but don't overdo it with DRY, some duplication is fine)
+* Single responsibility principle
+* Modularity and dependencies
+* API design and interfaces
+* Configuration management
+
+### PERFORMANCE
+* Algorithmic efficiency
+* Resource usage
+* Caching opportunities
+* Memory management
+
+### SECURITY
+* Input sanitization
+* Authentication/authorization
+* Data validation
+* Known vulnerability patterns
+
+### TESTING
+* Unit test coverage
+* Integration test needs
+* Edge case testing
+* Error scenario coverage
+
+### POSITIVE HIGHLIGHTS
+* Note any well-implemented patterns
+* Highlight good practices found
+* Commend effective solutions
+
+Format findings as markdown and with:
+- Issue: [description]
+- Impact: [specific impact]
+- Suggestion: [concrete improvement with code example/suggestion]
+
+              ]],
+                        },
+                        {
+                            role = "user",
+                            content = "Please review provided code.\n" .. "#buffer #lsp",
+                        },
+                    },
+                },
             },
         })
-
-        local keymap = vim.keymap.set
-
-        keymap({ "n", "v", "x" }, "<leader>cp", ":CodeCompanionActions<CR>")
-
-        keymap({ "n", "v" }, "<leader>ck", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-        keymap({ "n", "v" }, "<leader>ai", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-        keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
-
-        keymap("n", "<leader>cm", select_model, { desc = "Select Gemini Model" })
-        -- Expand 'cc' into 'CodeCompanion' in the command line
-        vim.cmd([[cab cc CodeCompanion]])
 
         local fidget = require("fidget")
         local handler
@@ -134,5 +339,27 @@ return {
                 end,
             })
         end
+    end,
+    keys = function()
+        return {
+            { "<leader>ac", ":CodeCompanionChat anthropic<CR>", desc = "Codecompanion: Claude" },
+            { "<leader>ao", ":CodeCompanionChat openai<CR>", desc = "Codecompanion: OpenAI" },
+            { "<leader>ag", ":CodeCompanionChat gemini<CR>", desc = "Codecompanion: Gemini" },
+            { "<leader>al", ":CodeCompanionChat ollama<CR>", desc = "Codecompanion: Ollama" },
+
+            { "<leader>at", ":CodeCompanionChat Toggle<CR>", desc = "Codecompanion toggle" },
+            {
+                "<leader>aS",
+                function()
+                    local name = vim.fn.input("Save as: ")
+                    if name and name ~= "" then
+                        vim.cmd("CodeCompanionSave " .. name)
+                    end
+                end,
+                desc = "Codecompanion Save chat",
+            },
+            { "<leader>aL", ":CodeCompanionLoad<CR>", desc = "Codecompanion Load chat" },
+            { "<leader>aP", ":CodeCompanionActions<CR>", desc = "Codecompanion Prompts" },
+        }
     end,
 }
